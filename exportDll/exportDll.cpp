6ae804901390e344b6c332b5ef2d5bc03c848184
yusisc <yusisc@gmail.com>
2022-08-21 14:06:19 +0800

    create function

---
+++ exportDll/exportDll.cpp
@@ -0,0 +1,16 @@
+//Exporting from a DLL Using __declspec(dllexport) | Microsoft Docs
+//https://docs.microsoft.com/en-us/cpp/build/exporting-from-a-dll-using-declspec-dllexport?view=msvc-170
+#include <iostream>
+
+#define DLLEXPORT __declspec(dllexport)
+
+DLLEXPORT int my_add(int aa, int bb)
+{
+	return aa + bb;
+}
+
+int main()
+{
+	std::cout << my_add(1, 2) << std::endl;
+	return 0;
+}
